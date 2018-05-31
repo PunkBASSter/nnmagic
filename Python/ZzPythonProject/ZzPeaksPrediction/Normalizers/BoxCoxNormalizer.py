d2f898e387c8896e40cee8eba954134e5804bae2
Schpielmann <Andrei_Shpilev@epam.com>
2018-05-31 19:11:44 +0300

    BoxCoxNormalizer updates

--- Python/ZzPythonProject/ZzPeaksPrediction/Normalizers/BoxCoxNormalizer.py
+++ Python/ZzPythonProject/ZzPeaksPrediction/Normalizers/BoxCoxNormalizer.py
@@ -6,6 +6,7 @@ from scipy import stats
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import warnings
+import datetime as dt
 import numpy as np
 
 from Common.ModelParameters import ModelParameters
@@ -17,23 +18,79 @@ def invboxcox(y,lmbda):
    else:
       return(np.exp(np.log(lmbda*y+1)/lmbda))
 
+
+def is_stationary(collection):
+   test = sm.tsa.stattools.adfuller(collection)
+   print('adf: ', test[0])
+   print('p-value: ', test[1])
+   print('Critical values: ', test[4])
+   res = False
+   if test[0] > test[4]['5%']:
+      print('есть единичные корни, ряд не стационарен')
+   else:
+      print('единичных корней нет, ряд стационарен')
+      res = True
+   return res
+
 params = ModelParameters
 
-quotes = pd.read_csv(params.io_input_data_file, index_col=['Timestamp'])
+
+quotes_df = pd.read_csv(params.io_input_data_file)
+
+quotes_df.reset_index()
+quotes_df['Date'] = pd.to_datetime(quotes_df['Timestamp'])
+quotes_df.set_index('Date')
+
+#dts = []
+#for ts in quotes_df["Timestamp"]:
+#   dts.append(dt.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+#for i in range(0, quotes_df["Timestamp"].__len__()):
+#   dts.append(dt.datetime.utcnow().date() + dt.timedelta(days=i))
+
+#quotes_df["FakeDate"] = pd.Series(dts)
+
+
+
 #wage['daily'] = wage.WAG_C_M*1.0 / wage.index.days_in_month
 #quotes.head()
 
-plt.figure(figsize=(15, 7))
-quotes.Value.plot()
+#plt.figure(figsize=(15, 7))
+#quotes_df.Value.plot()
 #wage.daily.plot()
-plt.ylabel('Values')
-plt.show()
+#plt.ylabel('Values')
+#plt.show()
 
-plt.figure(figsize=(15, 10))
-seasonal_dcs = sm.tsa.seasonal_decompose(quotes.Value)
+#plt.figure(figsize=(15, 10))
+seasonal_dcs = sm.tsa.seasonal_decompose(quotes_df.Value, freq=500)
+#seasonal_dcs.plot()
+df = sm.tsa.stattools.adfuller(quotes_df.Value)
 
-seasonal_dcs.plot()
-plt.ylabel('Values')
+
+
+
+#check for stationary - for differences also
+is_stationary(quotes_df.Value)
+print("\n")
+
+idiff = quotes_df.Value
+for i in range(2, 100, 2):
+   idiff = quotes_df.Value.diff(periods=i).dropna()
+   if is_stationary(idiff):
+      print("Autoregression period = "+str(i) + "\n")
+      break
+
+quotes_df['ValueBox'], lmbda = stats.boxcox(quotes_df.Value)
+plt.figure(figsize=(15, 7))
+quotes_df.Value.plot()
+quotes_df.ValueBox.plot()
+plt.ylabel(u'Transformed values')
+print("Оптимальный параметр преобразования Бокса-Кокса: %f" % lmbda)
+is_stationary(quotes_df.ValueBox)
+
+idiff.plot()
+
+plt.grid()
 #print("Критерий Дики-Фуллера: p=%f" % sm.tsa.stattools.adfuller(quotes.Value)[1])
 plt.show()
 
