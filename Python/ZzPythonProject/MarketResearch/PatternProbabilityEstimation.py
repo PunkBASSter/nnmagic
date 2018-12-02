3f11d2b5fe5cb5736b20b4a5a121f95d0315d339
schpielmann <stdrew90@gmail.com>
2018-12-02 23:48:07 +0300

    Added sequences and count of unique

--- Python/ZzPythonProject/MarketResearch/PatternProbabilityEstimation.py
+++ Python/ZzPythonProject/MarketResearch/PatternProbabilityEstimation.py
@@ -4,39 +4,50 @@ import math
 import datetime
 import numpy as np
 
-#header format: "timestamp,open,high,low,close,tick_volume,spread,real_vloume\n"
-#Specify your terminal folder
+#Expected Input file format: "timestamp,open,high,low,close,tick_volume,spread,real_volume\n"
+#Specify your terminal folder and filename
 path = 'C:\\Users\\PunkBASSter\\AppData\\Roaming\\MetaQuotes\\Terminal\\Common\\Files\\'
 file_name = 'Rates_EURUSD_PERIOD_H4.csv'
 
-bands_period = 20
-std_bands = [-2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5]
-ind_value_col = 'body'
+ind_period = 1 #Indicator mean period
+bands_period = 20 #Base MA period for Bands
+std_bands = [-2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5] #STD multipliers for bands
+ind_value_col = 'body' #Indicator Value column name
+sequence_len = 1 #Length of sequences of Indicator Values to analyse
 
+#Filling DataFrame with basic data, Indicator Values, MA, STD
 columns_to_take = ['timestamp', 'open', 'close']
 data = pd.read_csv(path+file_name)[columns_to_take]
-data['date'] = data.apply(lambda row: datetime.datetime.fromtimestamp(row['timestamp']), axis=1)
-data[ind_value_col] = data.close - data.open
+data['date'] = data.timestamp.apply(datetime.datetime.fromtimestamp)
+data[ind_value_col] = (data.close - data.open).rolling( ind_period).mean()
 data['ma'] = data[ind_value_col].rolling( bands_period ).mean()
 data['std'] = data[ind_value_col].rolling( bands_period ).std()
 
+#Calculating Bands of MA+K*STD
 for k in std_bands:
-    data[str(k)] = data['ma'] + data['std'].multiply(k)
+    data[f'Std_{k}'] = data['ma'] + data['std'].multiply(k)
+std_band_cols = [f'Std_{k}' for k in std_bands] #Bands column headers
 
-#Bands column headers
-k_str = [str(k) for k in std_bands]
-result_df = pd.DataFrame( data[[ind_value_col] + k_str], index=data.index )#.loc[bands_period - 1:]
-plt.figure()
-result_df.plot()
+#Visualizing Indicator
+result_df = pd.DataFrame( data[[ind_value_col] + std_band_cols], index=data.index )
+#plt.figure()
+#result_df.plot()
 
+#Defining a zone based on bands where Indicator Value is located
 def get_zone(std_cell_list : [], ind_func_res):
     bands_total = std_cell_list.__len__()
     for i in range(0, bands_total):
         if ind_func_res <= std_cell_list[i]:
             return i+1
     return bands_total + 1 if not math.isnan(std_cell_list[bands_total-1]) else math.nan
+result_df['zone'] = result_df.apply( lambda row: get_zone( [row[ki] for ki in std_band_cols], row[ind_value_col] ), axis=1 )
 
-result_df['zone'] = result_df.apply( lambda row: get_zone( [row[ki] for ki in k_str], row[ind_value_col] ), axis=1 )
+#Extracting Sequences for probability estimation
+seq_cols = [str(c) for c in range(sequence_len)]
+for c in seq_cols:
+    result_df[c] = result_df['zone'].shift(int(c))
 
-
+sequence_df = result_df.loc[ind_period -1 + bands_period-1 + sequence_len-1:, seq_cols].loc[:,::-1]
+sequence_df = sequence_df.groupby(sequence_df.columns.tolist()).size().reset_index(name='count')
+sequence_df['probability'] = sequence_df.apply(lambda row: lambda :row['count']/sequence_df.size(), axis=1)
 print('end')
