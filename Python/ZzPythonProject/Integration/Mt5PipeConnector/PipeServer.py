b65f31fd06d1bd00718cbbf0de144f7f01d78046
schpielmann <stdrew90@gmail.com>
2019-04-16 03:56:49 +0300

    Developing BotBase and PipeServer

--- Python/ZzPythonProject/Integration/Mt5PipeConnector/PipeServer.py
+++ Python/ZzPythonProject/Integration/Mt5PipeConnector/PipeServer.py
@@ -2,9 +2,10 @@ import time
 import sys
 import win32pipe, win32file, pywintypes
 import json
+from Integration.MtPyBotBase import MtPyBotBase
 
 
-def pipe_server():
+def pipe_server(process_str_callback):
     print("pipe server")
     count = 0
     pipe = win32pipe.CreateNamedPipe(
@@ -22,37 +23,25 @@ def pipe_server():
         while True:
             print(f"Reading message #{count}")
 
-            # convert bytes to json
             byte_content = win32file.ReadFile(pipe, 64*1024)[1]
             str_content = byte_content.decode("utf-8")
-            json_content = json.loads(str_content)
-            print(f"Received content: {str_content}")
 
-            
+            print(f"Received content: {str_content}. Calling processing_str_callback...")
+            processing_result = process_str_callback(str_content)
 
-
-            some_data = str.encode( f"{count % 4},1.{count},1.{count-1},1.{count+1},1,1544543200,")
+            #some_data = str.encode( f"{count % 4},1.{count},1.{count-1},1.{count+1},1,1544543200,")
 
             print(f"Writing data to pipe.")
-            win32file.WriteFile(pipe, some_data)
+            win32file.WriteFile(pipe, str.encode(processing_result))
             #win32file.flush()
+
             #time.sleep(1)
             count += 1
 
-        print("finished now")
-    #except:
-    #    pipe.send_error()
+
+    except:
+        pipe.send_error()
     finally:
         win32file.CloseHandle(pipe)
 
 
-if __name__ == '__main__':
-    #if len(sys.argv) < 2:
-    #    print("need s or c as argument")
-    #elif sys.argv[1] == "s":
-    #    pipe_server()
-    #elif sys.argv[1] == "c":
-    #    pipe_client()
-    #else:
-    #    print(f"no can do: {sys.argv[1]}")
-    pipe_server()
--- Python/ZzPythonProject/Integration/MtPyBotBase.py
+++ Python/ZzPythonProject/Integration/MtPyBotBase.py
@@ -1,5 +1,12 @@
 import json
 import pandas as pd
+from Integration.Mt5PipeConnector import PipeServer as pipe
+
+
+class StateStr:
+    InitStart = "init_start"
+    InitProgress = "init_progress"
+
 
 class MtPyBotBase:
     """Encapsulates basic API calls and structure of MT bot business logic"""
@@ -8,16 +15,33 @@ class MtPyBotBase:
     _data: pd.DataFrame
     _state: str
 
+    def __init__(self):
+        self._data = None
+        self._state = "init"
 
     def process_json_data(self, data_updates: str) -> str:
 
         json_dict = json.loads(data_updates)
         temp_df = pd.DataFrame(json_dict["data"])
-
+        #temp_df.reset_index()
+        temp_df = temp_df.set_index("timestamp")
+        #temp_df_t = pd.DataFrame(json_dict["data"]).transpose()
         if self._data is None:
             self._data = temp_df
         else:
-            self._data = self._data #  (MERGE)
+            df = pd.concat([self._data, temp_df]) #  (MERGE)
+            self._data = df[~df.index.duplicated(keep='last')]
 
-        pass
+        if temp_df.__len__() == 1:
+            self._data = self._data[~self._data.index.duplicated(keep='last')]
+            #init finished?
+            print("END!")
 
+        return "result"
+
+
+if __name__ == '__main__':
+
+    bot = MtPyBotBase()
+
+    pipe.pipe_server(bot.process_json_data)
